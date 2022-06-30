318fe81c3e1a4031369534091db4485f9d717e8b
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-07-01 02:15:06 +0300

    added style-loader sass-loader postcss-loader

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const PATHS = {
     src: path.join(__dirname, '../src'),
@@ -31,9 +32,38 @@ module.exports = {
             },
         },
     },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { esModule: false },
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true, url: false },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: { sourceMap: true, postcssOptions: { config: path.resolve(__dirname, "./postcss.config.js") } }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true },
+                    },
+                ],
+            },
+        ],
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: `${PATHS.src}/index.html`,
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].min.css',
         }),
         new CleanWebpackPlugin(),
     ],
