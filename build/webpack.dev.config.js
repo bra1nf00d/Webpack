adb0561b8826369071946563d49164f688b755b1
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-07-01 03:49:25 +0300

    added sourcemap

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,14 +1,20 @@
+const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.config');
 
 const devWebpackConfig = merge(baseWebpackConfig, {
     mode: 'development',
+    devtool: 'source-map',
     devServer: {
         port: 8080,
         hot: false,
         liveReload: true,
     },
-    plugins: [],
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+            filename: '[file].map',
+        }),
+    ],
 });
 
 module.exports = new Promise((resolve) => {
